0bf91504065a2408b11fc67a886216bb5bfb244e
Chicshin <inchicshin@gmail.com>
2019-07-24 16:18:28 +0900

    Implemented Firebase Database

---
+++ Facebook/ChicshinFirebaseFacebookAuth/ChicshinFirebaseFacebookAuth/UploadController.swift
@@ -0,0 +1,86 @@
+//
+//  UploadController.swift
+//  ChicshinFirebaseFacebookAuth
+//
+//  Created by Jane Shin on 7/24/19.
+//  Copyright © 2019 Jane Shin. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UploadController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var subject: UITextField!
+    @IBOutlet weak var explanation: UITextField!
+    @IBAction func submitButton(_ sender: Any) {
+        upload()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.addGestureRecognizer(UITapGestureRecognizer (target: self, action: #selector(openGallery)))
+        imageView.isUserInteractionEnabled = true
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func openGallery() {
+        
+        let imagePick = UIImagePickerController()
+        
+        imagePick.delegate = self
+        imagePick.allowsEditing = true
+        imagePick.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        self.present(imagePick, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        self.imageView.image = info [UIImagePickerController.InfoKey.originalImage] as! UIImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func upload(){
+        let image = (self.imageView.image)!.pngData()
+        let imageName = Auth.auth().currentUser!.uid + "\(Int(NSDate.timeIntervalSinceReferenceDate * 1000)).jpg"
+        
+        let riversRef = Storage.storage().reference().child("ios_image").child(imageName)
+        riversRef.putData(image!, metadata: nil) { (metadata, error) in
+            if (error != nil) {
+                
+            } else{
+
+                riversRef.downloadURL { (url, error) in
+                    if let downloadURL = url?.absoluteString{
+                        Database.database().reference().child("users").setValue([
+                            "userId" : Auth.auth().currentUser?.email,
+                            "uid" : Auth.auth().currentUser?.uid,
+                            "subject" : self.subject.text!,
+                            "explanation" : self.explanation.text!,
+                            "imageUrl" : downloadURL
+                            ])
+                    self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Facebook/ChicshinFirebaseFacebookAuth/ChicshinFirebaseFacebookAuth/UserController.swift
+++ Facebook/ChicshinFirebaseFacebookAuth/ChicshinFirebaseFacebookAuth/UserController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class UserController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class UserController: UIViewController {
 
     @IBAction func signout(_ sender: Any) {
         do {
@@ -19,46 +19,10 @@ class UserController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func profilePhotoUpload(_ sender: Any) {
-        
-        let imagePick = UIImagePickerController()
-        
-        imagePick.delegate = self
-        imagePick.allowsEditing = true
-        imagePick.sourceType = UIImagePickerController.SourceType.photoLibrary
-        
-        self.present(imagePick, animated: true, completion: nil)
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = (info [UIImagePickerController.InfoKey.originalImage] as! UIImage).pngData()
-        let imageName = Auth.auth().currentUser!.uid + "\(Int(NSDate.timeIntervalSinceReferenceDate * 1000)).jpg"
-        
-        let riversRef = Storage.storage().reference().child("ios_image").child(imageName)
-        riversRef.putData(image!, metadata: nil) { (metadata, error) in
-            guard let metadata = metadata else {
-                // Uh-oh, an error occurred!
-                return
-            }
-            // Metadata contains file metadata such as size, content-type.
-            let size = metadata.size
-            // You can also access to download URL after upload.
-            riversRef.downloadURL { (url, error) in
-                guard let downloadURL = url else {
-                    // Uh-oh, an error occurred!
-                    return
-                }
-            }
-        }
-        
-        dismiss(animated: true, completion: nil)
     }
     
 
