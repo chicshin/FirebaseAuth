d3ed2a547ba25e6a88db070b7cf346e66c2f1582
Chicshin <inchicshin@gmail.com>
2019-07-26 01:24:15 +0900

    Added RemoteConfig

--- Facebook/ChicshinFirebaseFacebookAuth/ChicshinFirebaseFacebookAuth/ViewController.swift
+++ Facebook/ChicshinFirebaseFacebookAuth/ChicshinFirebaseFacebookAuth/ViewController.swift
@@ -12,11 +12,34 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 
 class ViewController: UIViewController, LoginButtonDelegate  {
-
+    
     @IBOutlet weak var facebookLoginButton: FBLoginButton!
+    
+    var remoteConfg : RemoteConfig!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        remoteConfg = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfg.configSettings = settings
+        remoteConfg.setDefaults(fromPlist: "FireSwiftRemoteConfigDefaults")
+        
+        remoteConfg.fetch(withExpirationDuration: TimeInterval(0)) { (status, error) -> Void in
+            if status == .success {
+                print("Config fetched")
+                self.remoteConfg.activate(completionHandler: { (error) in
+                    
+                })
+            }else {
+                print("Config not fetched")
+            }
+            self.displayMessage()
+        
+        }
+        
         facebookLoginButton.delegate = self
         
         Auth.auth().addStateDidChangeListener({ (user, err) in
@@ -26,6 +49,30 @@ class ViewController: UIViewController, LoginButtonDelegate  {
             }
         })
         
+    }
+    
+    func displayMessage(){
+        
+        let message = remoteConfg["welcome_message"].stringValue
+        let caps = remoteConfg["welcome_message_caps"].boolValue
+        let color = remoteConfg["backgroundColor"].numberValue
+        
+        if(caps){
+            print("remote activated")
+            let alert = UIAlertController(title: "Notice", message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Confirm", style: UIAlertAction.Style.default, handler: { (action) in
+                exit(0)
+            }))
+            self.dismiss(animated: true) { () -> Void in
+                self.present(alert, animated: true, completion: nil)
+            }
+        }else{
+            print("Home Segue")
+            self.performSegue(withIdentifier: "Home", sender: nil)
+        }
+        if(color == 1){
+            self.view.backgroundColor = UIColor.gray
+        }
     }
     
     func loginButton(_ loginButton: FBLoginButton!, didCompleteWith result: LoginManagerLoginResult?, error: Error!) {
